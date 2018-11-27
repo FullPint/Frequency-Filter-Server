87914e530c3b7663c45147c43935fe9aa2725e93
fullPint <davila.alec@gmail.com>
2018-11-26 18:57:23 -0600

    first commit

---
+++ Filters/laplace_filter.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Laplace:
+    def __init__(self, shape):
+        self.p = shape[0]
+        self.q = shape[1]
+        self.filter = self.build_filter()
+
+    def build_filter(self):
+        filter = np.zeroes((self.p, self.q))
+        index_iterator = np.nditer(filter, flags=['multi_index'])
+        while not index_iterator.finished:
+            u = index_iterator.multi_index[0]
+            v = index_iterator.multi_index[1]
+            filter[u][v] = 1 + (np.square(u - self.p) + np.square(v - self.q))
+            index_iterator.iternext()
+        return filter
+
+    def get_laplace(self):
+        return self.filter
