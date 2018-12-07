9ee73449239fc0bc6d9e78a4795be8f92ad81cdb
fullPint <davila.alec@gmail.com>
2018-12-07 16:03:10 -0600

    fixed cutoff typo

--- Filtering_Server/Filters/gaussian_band_pass.py
+++ Filtering_Server/Filters/gaussian_band_pass.py
@@ -4,7 +4,7 @@ from .filter import Filter
 class GaussianBandPass(Filter):
     def __init__(self, shape, cutoff, width):
         super().__init__(shape)
-        self.cutoff = cuttoff
+        self.cutoff = cutoff
         self.width = width
 
     def build_filter(self):
