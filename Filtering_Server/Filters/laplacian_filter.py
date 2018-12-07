81027865c37969222752a1c59e4083cfa3f3422f
fullPint <davila.alec@gmail.com>
2018-12-07 16:20:38 -0600

    fixed laplacian formula

--- Filtering_Server/Filters/laplacian_filter.py
+++ Filtering_Server/Filters/laplacian_filter.py
@@ -11,6 +11,6 @@ class Laplace(Filter):
         while not index_iterator.finished:
             u = index_iterator.multi_index[0]
             v = index_iterator.multi_index[1]
-            mask[u][v] = 1 + (np.square(u - self.p) + np.square(v - self.q))
+            mask[u][v] = -(np.square(u - np.divide(self.p, 2)) + np.square(v - np.divide(self.q, 2)))
             index_iterator.iternext()
         self.mask = mask
