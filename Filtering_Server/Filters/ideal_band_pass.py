59ca8663558e845d737028fed5801878e4d40c5d
fullPint <davila.alec@gmail.com>
2018-12-06 22:14:22 -0600

    built out the ideal band pass and reject classes

--- Filtering_Server/Filters/ideal_band_pass.py
+++ Filtering_Server/Filters/ideal_band_pass.py
@@ -2,5 +2,21 @@ import numpy as np
 from .filter import Filter
 
 class IdealBandPass(Filter):
-    def __init__(self, shape):
-        Filter.__init__(self, shape)
+    def __init__(self, shape, cutoff, width):
+        super().__init__(shape)
+        self.cutoff = cutoff
+        self.width = width
+
+    def build_filter(self):
+        mask = np.zeros(self.shape)
+        index_iterator = np.nditer(mask, flags=['multi_index'])
+        while not index_iterator.finished:
+            u = index_iterator.multi_index[0]
+            v = index_iterator.multi_index[1]
+            distance = super().calculate_distance(u, v)
+            if distance >= self.cutoff-(self.width/2) and distance <= self.cutoff+(self.width/2):
+                mask[u][v] = 1
+            else:
+                mask[u][v] = 0
+            index_iterator.iternext()
+        self.mask = mask
--- Filtering_Server/Filters/ideal_band_reject.py
+++ Filtering_Server/Filters/ideal_band_reject.py
@@ -1,6 +1,10 @@
 import numpy as np
-from .filter import Filter
+from .ideal_band_pass import IdealBandPass
 
-class IdealBandReject(Filter):
-    def __init__(self, shape):
-        Filter.__init__(self, shape)
+class IdealBandReject(IdealBandPass):
+    def __init__(self, shape, cutoff, width):
+        super().__init__(shape, cutoff, width)
+
+    def build_filter(self):
+        super().build_filter()
+        self.mask = 1 - super().get_filter()
