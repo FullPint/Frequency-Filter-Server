0702c1d52b00cc74946694ea8702e3663568b360
fullPint <davila.alec@gmail.com>
2018-12-07 16:13:41 -0600

    defaulted calls to make all fitlers work with controller despite what the form contains

--- Filtering_Server/Front_End/Controllers/filtering_controller.py
+++ Filtering_Server/Front_End/Controllers/filtering_controller.py
@@ -6,14 +6,17 @@ import cv2
 app = current_app
 
 def filter_view():
-    path = os.path.join(app.config['FOO'],request.form['image_file'])
-    img = cv2.imread(path,0)
-    name = request.form['filter']
-    cutoff = np.int(request.form['cutoff_value'])
+    path = os.path.join(app.config['FOO'],request.form.get('image_file'))
+    image = cv2.imread(path,0)
+    filter_name = request.form.get('filter', 'laplacian_filter')
+    cutoff = np.int(request.form.get('cutoff', "0"))
+    a_value = np.int(request.form.get('a_value', "0"))
+    order = np.int(request.form.get('order', "0"))
+    width = np.int(request.form.get('width', "0"))
     implementation = request.form['fftImplemntation']
-    f = Filtering(img, name, cutoff, implementation=implementation)
+    f = Filtering(image, filter_name, cutoff, a_value, order, width, implementation)
     f.apply_filter()
-    filename = 'post_image_' + name + str(f.id) +  '.png'
+    filename = 'post_image_' + filter_name + str(f.id) +  '.png'
     post_image = f.filter_image
     cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], filename), post_image)
     return send_from_directory(app.config['UPLOAD_FOLDER'],
