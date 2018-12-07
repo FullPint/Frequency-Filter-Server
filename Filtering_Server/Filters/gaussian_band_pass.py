31d17573aa7fd645378c36cf0b98cb1c7220b7fb
fullPint <davila.alec@gmail.com>
2018-12-06 22:45:04 -0600

    finalized gaussian band filiters

--- Filtering_Server/Filters/gaussian_band_pass.py
+++ Filtering_Server/Filters/gaussian_band_pass.py
@@ -2,5 +2,19 @@ import numpy as np
 from .filter import Filter
 
 class GaussianBandPass(Filter):
-    def __init__(self, shape):
-        Filter.__init__(self, shape)
+    def __init__(self, shape, cutoff, width):
+        super().__init__(shape)
+
+    def build_filter(self):
+        mask = np.zeros(self.shape)
+        index_iterator = np.nditer(mask, flags=['multi_index'])
+        while not index_iterator.finished:
+            u = index_iterator.multi_index[0]
+            v = index_iterator.multi_index[1]
+            distance = super().calculate_distance(u, v)
+            if distance == 0:
+                mask[u][v] = 0
+            else:
+                mask[u][v] =  1 - np.exp(-(((distance ** 2 - self.cutoff ** 2) / (distance * self.width)) ** 2))
+            index_iterator.iternext()
+        self.mask = mask
--- Filtering_Server/Filters/gaussian_band_reject.py
+++ Filtering_Server/Filters/gaussian_band_reject.py
@@ -1,6 +1,10 @@
 import numpy as np
-from .filter import Filter
+from .gaussian_band_pass import GaussianBandPass
 
-class GaussianBandReject(Filter):
-    def __init__(self, shape):
-        Filter.__init__(self, shape)
+class GaussianBandReject(GaussianBandPass):
+    def __init__(self, shape, cutoff, width):
+        super().__init__(shape, cutoff, width)
+
+    def build_filter(self):
+        super().build_filter()
+        self.mask = 1 - super().get_filter()
