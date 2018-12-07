4fd30c166a370190c7d32cf16a0a1c0ae9498653
fullPint <davila.alec@gmail.com>
2018-12-06 21:28:37 -0600

    made filter as general as possible to be parent class

--- Filtering_Server/Filters/filter.py
+++ Filtering_Server/Filters/filter.py
@@ -1,17 +1,14 @@
-from numpy import np
+import numpy as np
 
 class Filter:
     def __init__(self, shape):
         self.shape = shape
         self.p = shape[0]
         self.q = shape[1]
-        self.filter = self.build_filter()
-
-    def build_filter(self):
-        return np.zeros(self.shape)
+        self.mask = np.zeros(shape)
 
     def get_filter(self):
-        return self.filter
+        return self.mask
 
     def calculate_distance(self, u, v):
         return np.sqrt((u - (self.p/ 2)) ** 2 + ((v - (self.q / 2)) ** 2))
