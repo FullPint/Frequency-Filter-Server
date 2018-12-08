9e32f327c1319f4f1ceadb6233938aa469dcdbf9
fullPint <davila.alec@gmail.com>
2018-12-07 18:19:38 -0600

    made some functions callable from Controller

---
+++ Filtering_Server/Front_End/Controllers/__init__.py
@@ -0,0 +1,4 @@
+from .confirm_controller import get_confirm
+from .filtering_controller import filter_view
+from .index_controller import get_index, post_index
+from .upload_controller import upload_file
