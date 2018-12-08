47776496e57fd584ae3fbf7084936b46f8274b29
fullPint <davila.alec@gmail.com>
2018-12-07 19:03:14 -0600

    Mad a result page that shows both the final image and the image in the frequency domain

--- Filtering_Server/Front_End/Controllers/filtering_controller.py
+++ Filtering_Server/Front_End/Controllers/filtering_controller.py
@@ -1,4 +1,4 @@
-from flask import url_for, request, current_app, send_from_directory
+from flask import url_for, request, current_app, send_from_directory, render_template
 from Filtering_Server.Filtering import Filtering
 import numpy as np
 import os
@@ -6,6 +6,19 @@ import cv2
 app = current_app
 
 def filter_view():
+    f = filter()
+    filter_image = 'post_image_' + f.filter_name + str(f.id) +  '.png'
+    cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], filter_image), f.filter_image)
+    filter_image = 'Images/' + filter_image
+    freq_image = 'freq_image_' + f.filter_name + str(f.id) + '.png'
+    cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], freq_image), f.filter_freq_image)
+    freq_image = 'Images/' + freq_image
+    return render_template('filter_complete.html',
+                            filter_image = filter_image,
+                            freq_image  = freq_image)
+    #return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               #filter_image)
+def filter():
     path = os.path.join(app.config['FOO'],request.form.get('image_file'))
     image = cv2.imread(path,0)
     filter_name = request.form.get('filter', 'laplacian_filter')
@@ -16,8 +29,4 @@ def filter_view():
     implementation = request.form['fftImplemntation']
     f = Filtering(image, filter_name, cutoff, a_value, order, width, implementation)
     f.apply_filter()
-    filename = 'post_image_' + filter_name + str(f.id) +  '.png'
-    post_image = f.filter_image
-    cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], filename), post_image)
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    return f
